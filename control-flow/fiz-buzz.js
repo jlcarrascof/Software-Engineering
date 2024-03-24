66e80bef5dd3d5bc854ed5165fdab0d4af470907
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-23 23:07:08 -0400

    Resolve fizBuzz algorithm

---
+++ control-flow/fiz-buzz.js
@@ -0,0 +1,15 @@
+function fixBuzz(max) {
+    let min = 0;
+    while (min+1 < max-1) {
+        min ++;
+        if (min % 3 === 0) {
+            if (min % 5 !== 0) {
+                console.log(min);
+            }
+        } else if (min % 5 === 0) {
+            console.log(min);
+        }
+    }
+}
+
+fixBuzz(20);
