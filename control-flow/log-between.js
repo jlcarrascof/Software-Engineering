c49b0ac069c6c0bbbc6c3926a18e1a05c3c7dd53
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-23 22:11:14 -0400

    Resolve logBetween algorithm

---
+++ control-flow/log-between.js
@@ -0,0 +1,10 @@
+function logBetween(lowNum, highNum) {
+    while (lowNum <= highNum) {
+        console.log(lowNum);
+        lowNum++;
+    }
+}
+
+logBetween(-1, 2);
+logBetween(14, 6);
+logBetween(4, 6);
