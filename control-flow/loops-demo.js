d9cf1f8da5c8d9769d5ce3ea633a592a916031d4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-14 12:32:42 -0400

    Finish exercise #1

--- control-flow/loops-demo.js
+++
@@ -1,7 +0,0 @@
-
-let index = 0;
-
-while (index < 5) {
-    console.log("Hello world!");
-    index++;
-}
--- control-flow/print-fives-1.js
+++
@@ -1,9 +0,0 @@
-function printFives(max) {
-    let i = 0;
-    while (i < max) {
-        console.log(i);
-        i += 5;
-    }
-}
-
-printFives(20);
