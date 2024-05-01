7a29c15e3d2a9a514a5d33df92b8943a3bcead49
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-01 18:52:46 -0400

    Create example3.js - Previous example, refactored

---
+++ non-standard-loops/example3.js
@@ -0,0 +1,7 @@
+const names = ["Rebecca", "Rex", "Priyanka", "Rory", "Kai", "Lana"];
+
+// Printing odd indexed names - refactored
+
+for (let i = 1; i < names.length; i+=2) {
+    console.log(names[i]);
+}
