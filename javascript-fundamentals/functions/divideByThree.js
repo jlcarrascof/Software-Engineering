c91399e178408c767a1477b87ddf5284249540a0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-15 18:47:04 -0400

    feat: Add divideByThree function to divide a number by three

---
+++ javascript-fundamentals/functions/divideByThree.js
@@ -0,0 +1,8 @@
+function divideByThree(num) {
+    // your code here...
+    return (num / 3);
+}
+
+console.log(divideByThree(9)); // => 3
+console.log(divideByThree(15)); // => 5
+console.log(divideByThree(98)); // => 32.666666666666664
