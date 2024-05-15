66391794cd51149b4baf0e4486d7531b5c2add42
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-15 18:23:21 -0400

    feat: Add hello function to greet a given name

---
+++ javascript-fundamentals/functions/hello.js
@@ -0,0 +1,8 @@
+function hello(str) {
+    // your code here...
+    console.log("Hello, " + str);
+}
+
+hello("cat"); // prints "Hello, cat"
+hello("Helen"); // prints "Hello, Helen"
+hello("Sting"); // prints "Hello, Sting"
