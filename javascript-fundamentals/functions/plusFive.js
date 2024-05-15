7c13189478b3aac469ebca1f1d400b38b580a7fe
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-15 11:35:14 -0400

    feat: Add plusFive function to add 5 to a given number

---
+++ javascript-fundamentals/functions/plusFive.js
@@ -0,0 +1,8 @@
+function plusFive(num) {
+    // your code here...
+    return num + 5;
+}
+
+console.log(plusFive(10));  // => 15
+console.log(plusFive(2));   // => 7
+console.log(plusFive(-8));  // => -3
