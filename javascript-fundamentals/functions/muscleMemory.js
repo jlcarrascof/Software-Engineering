29f0d8ffdcc9c91f5c2ba0d2efd42de638106f8d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-15 11:07:02 -0400

    Add muscleMemory.js file with sayHello and sayGoodbye functions

---
+++ javascript-fundamentals/functions/muscleMemory.js
@@ -0,0 +1,16 @@
+function sayHello(name) {
+    let msg = "Hello, " + name + ". How are you?";
+    return msg;
+}
+
+console.log(sayHello("world"));
+
+// Number 1
+function sayGoodbye(name) {
+    let msg = "Goodbye, " + name + ". See you soon."
+    return msg;
+}
+
+console.log(sayGoodbye('Peter'));
+
+// Number 2
