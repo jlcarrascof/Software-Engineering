c50d87009719832dd7b5265018d1ebb440460196
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-21 13:10:30 -0400

    feat: Add isSubstring function to check if a string contains a substring

---
+++ javascript-fundamentals/control-flow/isSubstring.js
@@ -0,0 +1,8 @@
+function isSubstring(searchString, subString) {
+    // your code here...
+    console.log(searchString.indexOf(subString));
+}
+
+console.log(isSubstring("The cat went to the store", "he cat went")); // => true
+console.log(isSubstring("Time to program", "time")); // => true
+console.log(isSubstring("Jump for joy", "joys")); // => false
