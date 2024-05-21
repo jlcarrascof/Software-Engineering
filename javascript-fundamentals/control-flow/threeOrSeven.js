deb886de35ff1a85aee7fb1f1b4f912be8107f42
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-21 13:40:39 -0400

    feat: Add threeOrSeven function to check if a number is divisible by three or seven

---
+++ javascript-fundamentals/control-flow/threeOrSeven.js
@@ -0,0 +1,7 @@
+function threeOrSeven(num) {
+    // your code here...
+}
+
+console.log(threeOrSeven(3));   // => true
+console.log(threeOrSeven(42));  // => true
+console.log(threeOrSeven(8));   // => false
