62c300496eea0df7dd613a4881ddd92c990747bb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-22 22:37:30 -0400

    Refactor logBetween function to accept two parameters for the range of numbers to print

--- javascript-fundamentals/basic-loops/logBetween.js
+++ javascript-fundamentals/basic-loops/logBetween.js
@@ -1,17 +1,29 @@
-function logBetween() {
+function logBetween(lowNum, highNum) {
+
+    // While version .
+
+    let init = lowNum;
+    while (init <= highNum) {
+        console.log(init);
+        init ++;
+    }
 
 }
 
 // Examples:
-logBetween(-1, 2); // prints out:
+logBetween(-1, 2);
+/* prints out:
 -1
 0
 1
 2
+*/
 
 logBetween(14, 6); // => prints nothing
 
-logBetween(4, 6); // prints out:
+logBetween(4, 6);
+/* prints out:
 4
 5
 6
+*/
