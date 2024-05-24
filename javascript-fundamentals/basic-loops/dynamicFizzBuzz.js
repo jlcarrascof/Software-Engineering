a121cda07b4e98112434b1c7293099bce272c754
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-24 13:08:19 -0400

    feat: Add dynamicFizzBuzz function to generate an array of numbers divisible by num1 or num2 up to a given maximum

---
+++ javascript-fundamentals/basic-loops/dynamicFizzBuzz.js
@@ -0,0 +1,10 @@
+function dynamicFizzBuzz(max, num1, num2) {
+
+    let resultArr = [];
+
+
+    return resultArr;
+}
+
+console.log(dynamicFizzBuzz(20, 3, 5)); // [ 3, 5, 6, 9, 10, 12, 18 ]
+console.log(dynamicFizzBuzz(24, 4, 6)); // [ 4, 6, 8, 16, 18, 20 ]
