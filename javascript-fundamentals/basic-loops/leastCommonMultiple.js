6f608bee1136733f88673608e2670757260491d6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-24 14:20:45 -0400

    Refactor leastCommonMultiple function for improved performance and readability

--- javascript-fundamentals/basic-loops/leastCommonMultiple.js
+++ javascript-fundamentals/basic-loops/leastCommonMultiple.js
@@ -1,3 +1,40 @@
 function leastCommonMultiple(num1, num2) {
 
+    let upperLimit = num1 * num2;
+
+    // Create the first list of elements.
+
+    array1 = [];
+    for (let i = 0; i <= upperLimit; i++) {
+        if (i % num1 ===0) {
+            array1.push(i);
+        }
+    }
+    console.log(array1)
+
+    // Create the second list of elements.
+
+    array2 = [];
+    for (let i = 0; i <= upperLimit; i++) {
+        if (i % num2 ===0) {
+            array2.push(i);
+        }
+    }
+    console.log(array2)
+
+    // Find MCM ....
+
+    let number = 0;
+    let check = false;
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== 0) {
+            check = array2.includes(array1[i]);
+            number = array1[i];
+            break;
+        }
+    }
+
+    console.log("MCM: " + number)
 }
+
+leastCommonMultiple(4, 6);
