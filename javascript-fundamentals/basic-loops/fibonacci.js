cbdef39b418307faca7ba5206ac0e395d65833d0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-24 22:43:32 -0400

    feat: Add Fibonacci sequence logic with tests

---
+++ javascript-fundamentals/basic-loops/fibonacci.js
@@ -0,0 +1,28 @@
+function logic(number) {
+    let values = [1, 1];
+
+    for (let i=0; i < number; i++) {
+        if (values.length > 2 || i >= 2) {
+            let ant1 = values[i-1];
+            let ant2 = values[i-2];
+            let nextValue = ant1 + ant2;
+            values.push(nextValue);
+        }
+    }
+
+    if (number === 0) {
+        values = [];
+        return values;
+    } else if (number === 1) {
+        values = [1];
+        return values;
+    } else {
+        return values;
+    }
+
+}
+
+console.log(logic(0)); // []
+console.log(logic(1)); // [1]
+console.log(logic(6)); // [1, 1, 2, 3, 5, 8]
+console.log(logic(8)); // [1, 1, 2, 3, 5, 8, 13, 21]
