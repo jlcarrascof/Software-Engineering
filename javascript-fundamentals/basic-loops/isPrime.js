e7322743f1d92a45b4c1f312354e151e95a8ab59
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-24 20:42:54 -0400

    refactor: Improve isPrime function performance and readability

--- javascript-fundamentals/basic-loops/isPrime.js
+++ javascript-fundamentals/basic-loops/isPrime.js
@@ -1,6 +1,21 @@
 function isPrime(number) {
 
+    if (number <= 1) {
+        return false;
+    }
 
+    let control = 0;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            control++;
+        }
+    }
+
+    if (control > 2) {
+        return false;
+    } else {
+        return true;
+    }
 
 }
 
