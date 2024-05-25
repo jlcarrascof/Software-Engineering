764731112334d47bd7288d8c23353a7890271925
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-24 22:02:32 -0400

    refactor: Improve isPerfectSquare function performance and readability

--- javascript-fundamentals/basic-loops/isPerfectSquare.js
+++ javascript-fundamentals/basic-loops/isPerfectSquare.js
@@ -1,16 +1,14 @@
 function isPerfectSquare(number) {
-
-    result = (Math.sqrt(number) % 1 === 0);
-
-    return result;
+    var squareNumber = Math.sqrt(number);
+    return Number.isInteger(squareNumber);
 }
 
-console.log(isPerfectSquare(1))     // true
-console.log(isPerfectSquare(4))     // true
-console.log(isPerfectSquare(64))    // true
-console.log(isPerfectSquare(100))   // true
-console.log(isPerfectSquare(169))   // true
-console.log(isPerfectSquare(2))     // false
-console.log(isPerfectSquare(40))    // false
-console.log(isPerfectSquare(32))    // false
-console.log(isPerfectSquare(50))    // false
+console.log(isPerfectSquare(1));    // true
+console.log(isPerfectSquare(4));    // true
+console.log(isPerfectSquare(64));   // true
+console.log(isPerfectSquare(100));  // true
+console.log(isPerfectSquare(169));  // true
+console.log(isPerfectSquare(2));    // false
+console.log(isPerfectSquare(40));   // false
+console.log(isPerfectSquare(32));   // false
+console.log(isPerfectSquare(50));   // false
