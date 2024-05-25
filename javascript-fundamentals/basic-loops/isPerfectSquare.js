76ccf0fe807b5cf0c3baeb96997cfa816d607456
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-24 21:22:22 -0400

    feat: Add isPerfectSquare function to check if a number is a perfect square

---
+++ javascript-fundamentals/basic-loops/isPerfectSquare.js
@@ -0,0 +1,13 @@
+function isPerfectSquare(num) {
+
+}
+
+console.log(isPerfectSquare(1))     // true
+console.log(isPerfectSquare(4))     // true
+console.log(isPerfectSquare(64))    // true
+console.log(isPerfectSquare(100))   // true
+console.log(isPerfectSquare(169))   // true
+console.log(isPerfectSquare(2))     // false
+console.log(isPerfectSquare(40))    // false
+console.log(isPerfectSquare(32))    // false
+console.log(isPerfectSquare(50))    // false
