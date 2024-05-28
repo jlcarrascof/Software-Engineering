17dd41e6b0feb9ba23a25c530d2e8b93da32fbd0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-28 12:42:15 -0400

    refactor: Optimize hasVowel function for better performance

--- javascript-fundamentals/string-loops/basic-string-loops/hasVowel.js
+++ javascript-fundamentals/string-loops/basic-string-loops/hasVowel.js
@@ -1,6 +1,18 @@
 function hasVowel(str) {
 
+    const myArray = str.split("");
+    const vowels = "aeiou".split("");
 
+    let hasVowels = false;
+
+    for (let i=0; i < myArray.length; i++) {
+        if (myArray.includes(vowels[i])) {
+            hasVowels = true;
+            break;
+        }
+    }
+
+    return hasVowels;
 
 }
 
