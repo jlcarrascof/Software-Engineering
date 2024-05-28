de9d43fbe2249c5fd1011a461b5f57df74801089
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-28 10:46:23 -0400

    Translate for loop to while loop in basic-string-loops/whileLoopTranslation.js

--- javascript-fundamentals/string-loops/basic-string-loops/whileLoopTranslation.js
+++ javascript-fundamentals/string-loops/basic-string-loops/whileLoopTranslation.js
@@ -15,3 +15,27 @@ console.log("Results using a for loop:");
 console.log(eCounter("apple")); // => 1
 console.log(eCounter("appleapple")); // => 2
 console.log(eCounter("Appleee")); // => 3
+
+// Translate the for loop above to a while loop below
+
+function wCounter(word) {
+
+    let count = 0;
+    let index = 0;
+
+    while (index < word.length) {
+        let char = word[index];
+        if (char === "e" || char === "E") {
+            count += 1;
+        }
+        index ++;
+    }
+
+    return count;
+
+}
+
+console.log("Results using a while loop:");
+console.log(wCounter("apple")); // => 1
+console.log(wCounter("appleapple")); // => 2
+console.log(wCounter("Appleee")); // => 3
