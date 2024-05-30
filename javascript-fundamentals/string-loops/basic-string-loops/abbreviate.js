edebdc9166b1bcfa54f4453da0a24e7cc6420c83
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 12:05:47 -0400

    feat: Add abbreviate function for basic string loops

---
+++ javascript-fundamentals/string-loops/basic-string-loops/abbreviate.js
@@ -0,0 +1,11 @@
+// your code here
+
+
+
+
+
+
+
+console.log(abbreviate('wonderful')); // 'wndrfl'
+console.log(abbreviate('mystery')); // 'mystry'
+console.log(abbreviate('Accordian')); // 'ccrdn'
