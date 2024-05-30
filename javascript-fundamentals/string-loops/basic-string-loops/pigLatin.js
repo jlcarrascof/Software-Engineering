3d4063b914e2b6757404e8f5a7af749667c2b6a4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 11:26:38 -0400

    refactor: Improve pigLatinWord function in pigLatin.js

--- javascript-fundamentals/string-loops/basic-string-loops/pigLatin.js
+++ javascript-fundamentals/string-loops/basic-string-loops/pigLatin.js
@@ -6,6 +6,29 @@
 
 function pigLatinWord(word) {
     // your code here...
+    // 1.- Verify is the word starts with a vowel
+
+    let vowels = "aeiou";
+    let finalWord = "";
+
+    if (vowels.includes(word[0])) {
+        finalWord = word + "yay";
+    } else {
+
+        let sliceWord = "";
+        let restWord = "";
+        for (let i=0; i < word.length; i++) {
+            if (vowels.includes(word[i]) || restWord.length > 0) {
+                restWord = restWord + word[i];
+            } else {
+                sliceWord = sliceWord + word[i];
+            }
+        }
+
+        finalWord = restWord + sliceWord + "ay";
+    }
+
+    return finalWord;
 
 };
 
