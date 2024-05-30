5a68ae48101fe3c1945da6ee026f8bef1b4711a9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 12:14:24 -0400

    Refactor abbreviate function for better readability and performance

--- javascript-fundamentals/string-loops/basic-string-loops/abbreviate.js
+++ javascript-fundamentals/string-loops/basic-string-loops/abbreviate.js
@@ -1,10 +1,16 @@
-// your code here
+function abbreviate(word) {
+    let vowels = "aeiou";
+    let finalWord = "";
 
+    for (let i=0; i < word.length; i++) {
+        if (!vowels.includes(word[i].toLowerCase())) {
+            finalWord = finalWord+word[i];
+        }
+    }
 
+    return finalWord;
 
-
-
-
+}
 
 console.log(abbreviate('wonderful')); // 'wndrfl'
 console.log(abbreviate('mystery')); // 'mystry'
