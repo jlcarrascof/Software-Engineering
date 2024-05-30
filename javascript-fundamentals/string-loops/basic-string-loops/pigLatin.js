9cae0d293afa7ea3345d371fc09c2dc52ef2b18c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 10:38:28 -0400

    feat: Add pigLatinWord function for basic string loops

---
+++ javascript-fundamentals/string-loops/basic-string-loops/pigLatin.js
@@ -0,0 +1,15 @@
+// So the two rules for our version of Pig Latin are:
+
+// 1. For words that start with a vowel, add 'yay' to the end of the word.
+// 2. For words that start with a non-vowel, move all letters that come
+// **before the first vowel** to the **end of the word** then add 'ay'
+
+function pigLatinWord(word) {
+    // your code here...
+
+};
+
+console.log(pigLatinWord("apple")); //=> "appleyay"
+console.log(pigLatinWord("eat")); //=> "eatyay"
+console.log(pigLatinWord("banana")); //=> "ananabay"
+console.log(pigLatinWord("trash")); //=> "ashtray"
