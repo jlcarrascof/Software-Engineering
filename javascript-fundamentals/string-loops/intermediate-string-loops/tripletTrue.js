fe0d237e518cb1497552ef11a0e92a8c27e54e9b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 20:51:26 -0400

    refactor: Improve tripletTrue function for better performance and readability

--- javascript-fundamentals/string-loops/intermediate-string-loops/tripletTrue.js
+++ javascript-fundamentals/string-loops/intermediate-string-loops/tripletTrue.js
@@ -1,6 +1,37 @@
 // Your code here
+function tripletTrue(str) {
+    let strAnt = "";
+    let strAct = "";
+    let count = 0;
+    let flag = false;
 
+    for (let i=0; i < str.length; i++) {
+        if (strAnt === "") {
+            strAnt = str[i];
+            count ++;
+        } else {
+            strAct = str[i];
+            if (strAct === strAnt) {
+                count ++;
+                strAnt = strAct;
+                if (count === 3) {
+                    flag = true;
+                    break;
+                }
+            } else {
+                strAnt = str[i];
+                count = 1;
+                flag = false;
+            }
+        }
+    }
 
+    if (flag) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 console.log(tripletTrue('caaabb'));        // true
 console.log(tripletTrue('terrrrrible'));   // true
