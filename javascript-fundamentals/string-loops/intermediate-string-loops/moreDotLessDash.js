764c808b048f94cd36afd5280eab10b1626393f1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 22:02:37 -0400

    feat: Add moreDotLessDash function for intermediate string loops

---
+++ javascript-fundamentals/string-loops/intermediate-string-loops/moreDotLessDash.js
@@ -0,0 +1,10 @@
+// Your code here
+function moreDotLessDash(str) {
+
+}
+
+console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
+console.log(moreDotLessDash('Morse code is great.'));                   // true
+console.log(moreDotLessDash('.... . -.--'));                            // true
+console.log(moreDotLessDash('.--. .-. --- --. .-. .- -- -- . .-.'));    // false
+console.log(moreDotLessDash('high-flying acrobat.'));                   // false
