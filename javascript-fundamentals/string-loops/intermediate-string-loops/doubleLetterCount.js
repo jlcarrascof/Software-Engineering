0944e42f2b6112878dec2fdb09cd173303601c17
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 18:36:36 -0400

    feat: Add doubleLetterCount function for counting the number of double letters in a string

---
+++ javascript-fundamentals/string-loops/intermediate-string-loops/doubleLetterCount.js
@@ -0,0 +1,6 @@
+function doubleLetterCount(string) {
+    // your code here
+}
+
+console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+console.log(doubleLetterCount("bootcamp")); // 1
