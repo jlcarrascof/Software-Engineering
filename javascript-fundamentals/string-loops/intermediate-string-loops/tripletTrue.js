71f0b90c3e4c246c1ced3e45117791db2c907356
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 20:59:37 -0400

    refactor: Refactor tripletTrue function to avoid redundances

--- javascript-fundamentals/string-loops/intermediate-string-loops/tripletTrue.js
+++ javascript-fundamentals/string-loops/intermediate-string-loops/tripletTrue.js
@@ -1,36 +1,27 @@
 // Your code here
 function tripletTrue(str) {
     let strAnt = "";
-    let strAct = "";
     let count = 0;
-    let flag = false;
 
     for (let i=0; i < str.length; i++) {
         if (strAnt === "") {
             strAnt = str[i];
             count ++;
         } else {
-            strAct = str[i];
-            if (strAct === strAnt) {
+            if (strAnt === str[i]) {
                 count ++;
-                strAnt = strAct;
+                strAnt = str[i];
                 if (count === 3) {
-                    flag = true;
-                    break;
+                    return true;
                 }
             } else {
                 strAnt = str[i];
                 count = 1;
-                flag = false;
             }
         }
     }
 
-    if (flag) {
-        return true;
-    } else {
-        return false;
-    }
+    return false;
 }
 
 console.log(tripletTrue('caaabb'));        // true
