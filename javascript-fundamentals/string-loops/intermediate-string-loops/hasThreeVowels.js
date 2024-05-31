60a6d7748079ad37d4b976616fa71ab0fd520906
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 14:55:35 -0400

    refactor: Simplify hasThreeVowels function for better readability and performance

--- javascript-fundamentals/string-loops/intermediate-string-loops/hasThreeVowels.js
+++ javascript-fundamentals/string-loops/intermediate-string-loops/hasThreeVowels.js
@@ -1,5 +1,32 @@
 // Your code here
 function hasThreeVowels(sentence) {
+    let control = 0;
+    let aFlag = false;
+    let eFlag = false;
+    let iFlag = false;
+    let oFlag = false;
+    let uFlag = false;
+
+    for (let i=0; i < sentence.length; i++) {
+        if (sentence[i] === 'a' && aFlag === false) {
+            control ++;
+            aFlag = true;
+        } else if (sentence[i] === 'e' && eFlag === false) {
+            control ++;
+            eFlag = true;
+        } else if (sentence[i] === 'i' && iFlag === false) {
+            control ++;
+            iFlag = true;
+        } else if (sentence[i] === 'o' && oFlag === false) {
+            control ++;
+            oFlag = true;
+        } else if (sentence[i] === 'u' && uFlag === false) {
+            control ++;
+            uFlag = true;
+        }
+    }
+
+    return control >= 3;
 
 }
 
