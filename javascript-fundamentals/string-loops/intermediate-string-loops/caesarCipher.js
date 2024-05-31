0d2e629107ce008d9d58804db3463148a0137c89
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 18:53:50 -0400

    feat: Implement caesarCipher function for intermediate string loops

---
+++ javascript-fundamentals/string-loops/intermediate-string-loops/caesarCipher.js
@@ -0,0 +1,10 @@
+// Feel free to use this variable:
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function caesarCipher(string, num) {
+    // your code here
+}
+
+console.log(caesarCipher("apple", 1)); // "bqqmf"
+console.log(caesarCipher("bootcamp", 2)); // "dqqvecor"
+console.log(caesarCipher("zebra", 4)); // "difve"
