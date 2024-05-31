74aae41565492e803ef4ef06fcc9d067e824cec6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 22:10:34 -0400

    refactor: Improve moreDotLessDash function for better performance and readability

--- javascript-fundamentals/string-loops/intermediate-string-loops/moreDotLessDash.js
+++ javascript-fundamentals/string-loops/intermediate-string-loops/moreDotLessDash.js
@@ -1,6 +1,20 @@
 // Your code here
 function moreDotLessDash(str) {
 
+    let sumDots = 0;
+    let sumDashes = 0;
+
+    for (let i=0; i < str.length; i++) {
+        if (str[i] === '-') {
+            sumDashes++;
+        }
+        if (str[i] === '.') {
+            sumDots++;
+        }
+    }
+
+    return sumDots > sumDashes;
+
 }
 
 console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
