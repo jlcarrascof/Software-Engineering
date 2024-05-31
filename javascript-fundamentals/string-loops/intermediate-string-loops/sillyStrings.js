5ab7e34266cc08f52cd87e5ea671e751988cc2b2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-30 21:11:31 -0400

    feat: Add sillyString function for manipulating strings in a silly way

---
+++ javascript-fundamentals/string-loops/intermediate-string-loops/sillyStrings.js
@@ -0,0 +1,11 @@
+// Your code here
+function sillyString(str) {
+
+}
+
+console.log(sillyString('stop'));       // stobop
+console.log(sillyString('that'));       // thabat
+console.log(sillyString('can'));        // caban
+console.log(sillyString('cats'));       // cabats
+console.log(sillyString('italy'));      // ibitabaly
+console.log(sillyString('scooter'));    // scobooboteber
