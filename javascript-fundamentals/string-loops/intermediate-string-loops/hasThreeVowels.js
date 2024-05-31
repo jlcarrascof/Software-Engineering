262b6704df74c9f8e1e178b8a584b48204aa4182
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 13:56:11 -0400

    feat: Implement hasThreeVowels function for intermediate string loops

---
+++ javascript-fundamentals/string-loops/intermediate-string-loops/hasThreeVowels.js
@@ -0,0 +1,10 @@
+// Your code here
+function hasThreeVowels(sentence) {
+
+}
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
