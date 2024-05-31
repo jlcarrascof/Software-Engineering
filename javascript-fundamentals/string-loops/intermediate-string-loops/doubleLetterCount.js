bf69acf91699fef9d4be4f3ee10eefcb3142c38a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 18:47:11 -0400

    refactor: Improve doubleLetterCount function and resolve for better performance and readability

--- javascript-fundamentals/string-loops/intermediate-string-loops/doubleLetterCount.js
+++ javascript-fundamentals/string-loops/intermediate-string-loops/doubleLetterCount.js
@@ -1,5 +1,13 @@
 function doubleLetterCount(string) {
     // your code here
+    let consecutiveCount = 0;
+    for (let i = 0; i < string.length-1; i++) {
+        if (string[i] === string[i+1]) {
+            consecutiveCount ++;
+        }
+    }
+
+    return consecutiveCount;
 }
 
 console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
