7d17b5cacc6c465e2d18b2b54d3ac9541b7bba98
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 18:18:58 -0400

    feat: Add lastIndex function for intermediate string loops

---
+++ javascript-fundamentals/string-loops/intermediate-string-loops/lastIndex.js
@@ -0,0 +1,8 @@
+function lastIndex(str, char) {
+    // your code here
+}
+
+console.log(lastIndex("abca", "a"))        // 3
+console.log(lastIndex("mississipi", "i"))  // 9
+console.log(lastIndex("octagon", "o"))     // 5
+console.log(lastIndex("programming", "m")) // 7
