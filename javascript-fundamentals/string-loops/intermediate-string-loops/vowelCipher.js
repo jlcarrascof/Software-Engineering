3a0ecb399bf954fff93f13d180d0d193b25c2004
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-31 22:46:28 -0400

    refactor: Improve and resolve vowelCipher function for better performance and readability

--- javascript-fundamentals/string-loops/intermediate-string-loops/vowelCipher.js
+++ javascript-fundamentals/string-loops/intermediate-string-loops/vowelCipher.js
@@ -2,12 +2,24 @@ function vowelCipher(string) {
     // your code here
     const vowels = "aeiou";
     let finalString = "";
+    let positionVowels = "";
 
     for (let i=0; i < string.length; i++) {
-        if (vowels.includes(string[i])) {
-            console.log(string[i]);
+        if (vowels.includes(string[i].toLowerCase())) {
+            positionVowels = vowels.indexOf(string[i]);
+            if (positionVowels < 4) {
+                positionVowels++;
+                finalString += vowels[positionVowels];
+            } else if (positionVowels === 4) {
+                positionVowels = 0;
+                finalString += vowels[positionVowels];
+            }
+        } else {
+            finalString += string[i];
         }
     }
+
+    return finalString.toLowerCase();
 }
 
 console.log(vowelCipher("bootcamp")); // "buutcemp"
