75e83a9e1823705b4b739c69f94fd88a4aa1dfb6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-03 12:55:23 -0400

    feat: Add moveZeroes function for moving zeroes to the end of an array

---
+++ javascript-fundamentals/problem-solving-II/moveZeroes.js
@@ -0,0 +1,8 @@
+function moveZeroes(nums) {
+    // 1. Set a pointer to the last element in the array.
+
+    // 2. Walk through each element of the array.
+
+    // 3. If the element is zero, swap its position with the element at the pointer,
+    // then decrement the pointer.
+}
