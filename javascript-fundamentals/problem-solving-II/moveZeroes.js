e7623c3089106a90d199a05457d6ea20c2685b54
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-03 13:05:55 -0400

    refactor: Optimize moveZeroes function for better performance and readability

--- javascript-fundamentals/problem-solving-II/moveZeroes.js
+++ javascript-fundamentals/problem-solving-II/moveZeroes.js
@@ -1,8 +1,18 @@
 function moveZeroes(nums) {
     // 1. Set a pointer to the last element in the array.
+    let last = nums.length - 1
 
     // 2. Walk through each element of the array.
+    for (let i = 0; i < last; i++) {
+        // 3. If the element is zero, swap its position with the element at the pointer,
+        // then decrement the pointer.
+        if (nums[i] === 0) {
+            [nums[i], nums[last]] = [nums[last], nums[i]];
+            last--;
+        }
+    }
 
-    // 3. If the element is zero, swap its position with the element at the pointer,
-    // then decrement the pointer.
+    return nums;
 }
+
+console.log(moveZeroes([0, 1, 0, 3, 12])); // [1, 3, 12, 0, 0]
