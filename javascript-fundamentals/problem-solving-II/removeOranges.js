117a0992c90523cf6146129568923770bf07b341
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-03 20:51:50 -0400

    refactor: Remove duplicate oranges from fruitArray in removeOranges function

--- javascript-fundamentals/problem-solving-II/removeOranges.js
+++ javascript-fundamentals/problem-solving-II/removeOranges.js
@@ -1,10 +1,11 @@
 function removeOranges(fruitArray) {
     for (let i = 0; i < fruitArray.length; i++) {
-     // If the item at the ith position is "orange"
-     if (fruitArray[i] == "orange") {
-      // Remove 1 item from the ith position
-      fruitArray.splice(i, 1);
-     }
+      console.log("Checking index " + i + ": " + fruitArray[i]);
+
+      if (fruitArray[i] == "orange") {
+        fruitArray.splice(i, 1);
+        i--;
+      }
     }
     return fruitArray;
 }
