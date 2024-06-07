cf3a55b5886a5285c08ebc8d08ab18d68258a3c6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-07 12:03:19 -0400

    refactor: Add combineArray function for array manipulation

---
+++ javascript-fundamentals/introduction-to-arrays/addArrays.js
@@ -0,0 +1,8 @@
+function combineArray(array1, array2) {
+    // your code here...
+
+
+}
+
+console.log(combineArray([1, 2], [3, 4])); // => [1, 2, 3, 4]
+console.log(combineArray([17, 5], [6, 7]));  // => [17, 5, 6, 7]
