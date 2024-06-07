4e8cfc155ff86a17ca096c5cab03a7fef78d4d81
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-07 11:10:52 -0400

    refactor: Add firstAndLast function for array manipulation

---
+++ javascript-fundamentals/introduction-to-arrays/firstAndLast.js
@@ -0,0 +1,9 @@
+function firstAndLast(arr) {
+
+}
+
+console.log(firstAndLast([1, 2, 3, 4]));    // 5
+console.log(firstAndLast([1, 2, 3, 4, 5])); // -4
+console.log(firstAndLast([12, 5]));         // 17
+console.log(firstAndLast([12]));            // 0
+console.log(firstAndLast([7, 11, 3]));      // 4
