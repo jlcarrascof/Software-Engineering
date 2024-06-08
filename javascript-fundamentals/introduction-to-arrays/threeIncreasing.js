6ac3845e0b5c08ffd8b3e16bb446bd1e75d716d2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-08 12:34:29 -0400

    refactor: Implement threeIncreasing function for array manipulation

---
+++ javascript-fundamentals/introduction-to-arrays/threeIncreasing.js
@@ -0,0 +1,8 @@
+function threeIncreasing(arr) {
+
+}
+
+console.log(threeIncreasing([3, 2, 11, 12, 13, 2, 4]));     // true
+console.log(threeIncreasing([2, 7, 8, 9]));                 // true
+console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6]));        // false
+console.log(threeIncreasing([1, 2, 4, 5, 2, 7, 8]));        // false
