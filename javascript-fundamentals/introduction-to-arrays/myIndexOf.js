021bb4d1ac4077870aaaf35eb43a40cbf75b8203
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-08 18:31:54 -0400

    refactor: Implement myIndexOf function for array searching

---
+++ javascript-fundamentals/introduction-to-arrays/myIndexOf.js
@@ -0,0 +1,9 @@
+function myIndexOf(arr, target) {
+
+}
+
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
+console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
+console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
