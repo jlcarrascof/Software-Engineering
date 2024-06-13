5e3a84bb165114e48ba19dbe64015003329d7b5f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-13 14:16:29 -0400

    feat: Add yelledStrings function for string manipulation

---
+++ javascript-fundamentals/intermediate-functions/yelledStrings.js
@@ -0,0 +1,16 @@
+function yellStr(str) {
+    let upperString = str.toUpperCase();
+    return upperString + '!';
+}
+
+function yelledStrings(strings) {
+    let yelled = [];
+
+    for (let i = 0; i < strings.length; i++) {
+      let string = strings[i];
+      let newString = yellStr(string);
+      yelled.push(newString);
+    }
+
+    return yelled;
+}
