b5073f7eb1d5ff233bdbacc89b9bc87baf7b1081
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-19 17:48:53 -0400

    feat: Implement isPrime function for prime number determination

--- javascript-fundamentals/intermediate-functions/isPrimeRecall.js
+++ javascript-fundamentals/intermediate-functions/isPrimeRecall.js
@@ -1,1 +1,11 @@
 // Determine if a number is prime.
+
+function isPrime(number) {
+
+}
+
+console.log(isPrime(2)); // => true
+console.log(isPrime(10)); // => false
+console.log(isPrime(11)); // => true
+console.log(isPrime(9)); // => false
+console.log(isPrime(2017)); // => true
