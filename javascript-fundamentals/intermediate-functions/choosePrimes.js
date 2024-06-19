a48eec07dfcd0585b5d3cd67f26db89764168402
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-19 18:23:28 -0400

    refactor: Create choosePrimes.js file

---
+++ javascript-fundamentals/intermediate-functions/choosePrimes.js
@@ -0,0 +1,21 @@
+function isPrime(number) {
+
+    if (number < 2) {
+        return false;
+    }
+
+    if (number === 2) {
+        return true;
+    }
+
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+
+        if (number % i === 0) {
+            return false;
+        }
+
+    }
+
+    return true;
+
+}
