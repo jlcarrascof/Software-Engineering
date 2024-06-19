005197721348489e236fec136f6b71824a14e336
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-19 18:24:30 -0400

    refactor: Implement choosePrimes function for selecting prime numbers from an array

--- javascript-fundamentals/intermediate-functions/choosePrimes.js
+++ javascript-fundamentals/intermediate-functions/choosePrimes.js
@@ -19,3 +19,10 @@ function isPrime(number) {
     return true;
 
 }
+
+function choosePrimes(arr) {
+
+}
+
+console.log(choosePrimes([36, 48, 9, 13, 19])); // [ 13, 19 ]
+console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
