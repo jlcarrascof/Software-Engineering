901c2558c33be6d37450f06f99cbde778684c4f7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 17:50:57 -0400

    refactor: Remove unnecessary console.log statements in mostVowels function

--- javascript-fundamentals/intermediate-functions/mostVowels.js
+++ javascript-fundamentals/intermediate-functions/mostVowels.js
@@ -28,5 +28,3 @@ function mostVowels(sentence) {
 }
 
 console.log(mostVowels("what a wonderful life")); // "wonderful"
-console.log(mostVowels("hello world")); // "hello"
-console.log(mostVowels("I am a keeper with some real rhythms")); // "rhythms"
