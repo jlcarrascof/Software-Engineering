8004f7820a7e20105ca3832ba33a6174e25656f2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 17:58:35 -0400

    refactor: Improve allElseEqual function for finding the element that is equal to all other elements in an array

--- javascript-fundamentals/intermediate-functions/allElseEqual.js
+++ javascript-fundamentals/intermediate-functions/allElseEqual.js
@@ -1,5 +1,17 @@
 function allElseEqual(arr) {
 
+    let sum = arr.reduce((acc, num) => acc + num, 0);
+
+    let halfSum = sum / 2;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === halfSum) {
+            return arr[i];
+        }
+    }
+
+    return null;
+
 }
 
 console.log(allElseEqual([2, 4, 3, 10, 1])); // 10
