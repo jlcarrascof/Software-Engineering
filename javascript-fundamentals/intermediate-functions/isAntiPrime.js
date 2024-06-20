087cc29264524bf416b76a3442ccf186b29059d2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 12:43:14 -0400

    refactor: Update isAntiPrime function to count divisors instead of checking for primality

--- javascript-fundamentals/intermediate-functions/isAntiPrime.js
+++ javascript-fundamentals/intermediate-functions/isAntiPrime.js
@@ -1,27 +1,14 @@
-function isPrime(number) {
-
-    if (number < 2) {
-        return false;
-    }
-
-    if (number === 2) {
-        return true;
-    }
-
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-
-        if (number % i === 0) {
-            return false;
+function countDivisors(num) {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            count++;
         }
-
     }
-
-    return true;
-
+    return count;
 }
 
-
-function isAntiPrime(num) {
+function isAntiPrime(number) {
 
 }
 
