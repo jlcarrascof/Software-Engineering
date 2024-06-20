215dc0205956793b4718a192f92fe1f3d7407092
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 17:53:20 -0400

    feat: Add allElseEqual function to find the element that is equal to all other elements in an array

---
+++ javascript-fundamentals/intermediate-functions/allElseEqual.js
@@ -0,0 +1,7 @@
+function allElseEqual(arr) {
+
+}
+
+console.log(allElseEqual([2, 4, 3, 10, 1])); // 10
+console.log(allElseEqual([6, 3, 5, -9, 1])); // 3
+console.log(allElseEqual([1, 2, 3, 4]));     // null
