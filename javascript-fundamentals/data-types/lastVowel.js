ba3edf1bbbb4566d977f40ad2037a9b1be3dd22a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-28 19:39:00 -0400

    feat: Add lastVowel function to find the last vowel in a string

---
+++ javascript-fundamentals/data-types/lastVowel.js
@@ -0,0 +1,10 @@
+// your code here
+function lastVowel(str) {
+
+}
+
+console.log(lastVowel('battery')); // 'e'
+console.log(lastVowel('TUNNEL')); // 'E'
+console.log(lastVowel('dog')); // 'o'
+console.log(lastVowel('conventional')); // 'a'
+console.log(lastVowel('rhythm')); // null
