1dbd2d2a52887622a4fca32a5e92b50f274f54d0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-28 19:03:09 -0400

    feat: Add firstVowel function to find the first vowel in a string

---
+++ javascript-fundamentals/data-types/firstVowel.js
@@ -0,0 +1,10 @@
+// your code here
+function firstVowel(str) {
+
+}
+
+console.log(firstVowel('battery')); // 'a'
+console.log(firstVowel('tunnel')); // 'u'
+console.log(firstVowel('dog')); // 'o'
+console.log(firstVowel('conventional')); // 'o'
+console.log(firstVowel('rhythm')); // null
