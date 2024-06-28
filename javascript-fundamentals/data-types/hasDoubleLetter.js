4b62bf2e82bb61657f24bb4be206146d0b251693
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-28 18:18:55 -0400

    feat: Implement hasDoubleLetter function for checking if a string has consecutive double letters

---
+++ javascript-fundamentals/data-types/hasDoubleLetter.js
@@ -0,0 +1,12 @@
+// your code here
+function hasDoubleLetter(str) {
+
+}
+
+console.log(hasDoubleLetter('deer')); // true
+console.log(hasDoubleLetter('boot camp')); // true
+console.log(hasDoubleLetter('toggle')); // true
+console.log(hasDoubleLetter('taco')); // false
+console.log(hasDoubleLetter('jumper')); // false
+console.log(hasDoubleLetter(18)); // null
+console.log(hasDoubleLetter(['array'])); // null
