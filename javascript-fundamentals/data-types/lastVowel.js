a2b4eb64e349b27a89d1d3899e50b68a35e92b52
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-28 19:55:24 -0400

    refactor: Improve and resolve lastVowel function for better performance

--- javascript-fundamentals/data-types/lastVowel.js
+++ javascript-fundamentals/data-types/lastVowel.js
@@ -1,6 +1,20 @@
 // your code here
 function lastVowel(str) {
 
+    let vowels = "aeiouAEIOU";
+    let lastVowel = "";
+    for (let i=0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            lastVowel = str[i];
+        }
+    }
+
+    if (lastVowel !== "") {
+        return lastVowel;
+    } else {
+        return null;
+    }
+
 }
 
 console.log(lastVowel('battery')); // 'e'
