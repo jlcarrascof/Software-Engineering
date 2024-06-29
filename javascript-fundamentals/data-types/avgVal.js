6d7004e10396835b718be99898184be8bfc6de12
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-29 18:43:59 -0400

    feat: Implement avgVal function to calculate the average value of an array

---
+++ javascript-fundamentals/data-types/avgVal.js
@@ -0,0 +1,13 @@
+// your code here
+function avgVal(arr) {
+
+    if (arr.length === 0) {
+        return null;
+    }
+
+
+}
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
