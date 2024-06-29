c0d0aaa7a9b015993d277ef746b4f7dc6109c372
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-29 19:10:09 -0400

    feat: Add maxValue function to find the maximum value in an array

---
+++ javascript-fundamentals/data-types/maxValue.js
@@ -0,0 +1,8 @@
+function maxValue(arr) {
+    // your code here
+
+}
+
+console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxValue([-2, -3, -7, 3 ])); // 3
+console.log(maxValue([])); // null"
