2dc2fb7c119aeaeaba8673c21ae50c9cf2ce3078
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-28 21:00:39 -0400

    feat: Add minValue function to find the minimum value in an array

---
+++ javascript-fundamentals/data-types/minValue.js
@@ -0,0 +1,8 @@
+// your code here
+function minValue(arr) {
+
+}
+
+console.log(minValue([4, 6, 3, 5, 2, 4])); // 2
+console.log(minValue([-2, -3, -7, 3 ])); // -7
+console.log(minValue([])); // null
