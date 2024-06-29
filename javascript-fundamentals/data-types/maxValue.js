39d47bc048652eb2f2e96dcee493e440c0ae9e2b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-29 19:17:35 -0400

    refactor: Improve maxValue function for better performance

--- javascript-fundamentals/data-types/maxValue.js
+++ javascript-fundamentals/data-types/maxValue.js
@@ -5,8 +5,18 @@ function maxValue(arr) {
         return null;
     }
 
+    let bigger = arr[0];
+
+    for (let i=0; i < arr.length; i++) {
+        if (bigger < arr[i+1]) {
+            bigger = arr[i+1];
+        }
+    }
+
+    return bigger;
+
 }
 
 console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
 console.log(maxValue([-2, -3, -7, 3 ])); // 3
-console.log(maxValue([])); // null"
+console.log(maxValue([])); // null
