6ca42b474bae2fcdc5a9a31490c36431935621b8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-28 21:28:21 -0400

    refactor: Improve and resolve minValue function for better performance

--- javascript-fundamentals/data-types/minValue.js
+++ javascript-fundamentals/data-types/minValue.js
@@ -1,6 +1,19 @@
 // your code here
 function minValue(arr) {
 
+    if (arr.length === 0) {
+        return null;
+    }
+
+    min = arr[0];
+
+    for (let i=0; i < arr.length; i++) {
+        if (min > arr[i+1]) {
+            min = arr[i+1];
+        }
+    }
+
+    return min;
 }
 
 console.log(minValue([4, 6, 3, 5, 2, 4])); // 2
