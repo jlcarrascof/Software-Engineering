cf1cc307bfd21729fdb2fc9b7a038d5b2f43fa87
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-30 19:47:21 -0400

    feat: Add reverb function to add extra characters to a string

---
+++ javascript-fundamentals/data-types/reverb.js
@@ -0,0 +1,9 @@
+function reverb(str) {
+
+}
+
+console.log(reverb('running')); // runninging
+console.log(reverb('FAMILY'));  // FAMILYILY
+console.log(reverb('trash'));   // trashash
+console.log(reverb('DISH'));    // DISHISH
+console.log(reverb(197393));    // null
