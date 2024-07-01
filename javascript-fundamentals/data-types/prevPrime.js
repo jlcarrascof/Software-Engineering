37415c5a963deac21a44148d7356bfb7bb6ca123
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-30 21:18:08 -0400

    feat: Add prevPrime function to find the previous prime number

---
+++ javascript-fundamentals/data-types/prevPrime.js
@@ -0,0 +1,11 @@
+function prevPrime(num) {
+
+}
+
+console.log(prevPrime(32)); // 31
+console.log(prevPrime(33)); // 31
+console.log(prevPrime(14)); // 13
+console.log(prevPrime(7));  // 5
+console.log(prevPrime(4));  // 3
+console.log(prevPrime(2));  // null
+console.log(prevPrime(1));  // null
