7e23ff72c182ea523cf58f0aeb04961d5c69a095
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-07 19:25:47 -0400

    feat: Implement alternatingWords function to alternate the case of words in an array

---
+++ javascript-fundamentals/data-types/alternatingWords.js
@@ -0,0 +1,10 @@
+// Your code here
+
+
+let words1 = ['Belka', 'STRELKA', 'laika', 'DEZIK', 'Tsygan'];
+alternatingWords(words1);
+console.log(words1); // [ 'BELKA', 'strelka', 'LAIKA', 'dezik', 'TSYGAN' ]
+
+let words2 = ['Yellowstone', 'Yosemite', 'Zion', 'Acadia'];
+alternatingWords(words2);
+console.log(words2); // [ 'YELLOWSTONE', 'yosemite', 'ZION', 'acadia' ]
