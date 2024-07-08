2db880d5d9c68b9ab8e3506e09a3fe50348f2792
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-07 21:23:57 -0400

    refactor: Simplify reverseString function for better performance and readability

---
+++ javascript-fundamentals/data-types/reverseStringSimplified.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString('fish')); // 'hsif'
+console.log(reverseString('marathon')); // 'nohtaram'
