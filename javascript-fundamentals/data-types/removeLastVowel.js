6a1ac46f0f7a9f406a66154ac17308e6f87a9bbe
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-07 23:03:53 -0400

    feat: Add removeLastVowel function to remove the last vowel from a word

---
+++ javascript-fundamentals/data-types/removeLastVowel.js
@@ -0,0 +1,9 @@
+// your code here
+function removeLastVowel(word) {
+
+}
+
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
