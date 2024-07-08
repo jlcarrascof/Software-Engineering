e8b746922d687d98ff4f097046ca8d4db95cd791
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-07 23:20:12 -0400

    refactor: Improve removeLastVowel function for better performance and readability

--- javascript-fundamentals/data-types/removeLastVowel.js
+++ javascript-fundamentals/data-types/removeLastVowel.js
@@ -1,6 +1,26 @@
 // your code here
 function removeLastVowel(word) {
 
+    let vowels = "aeiouAEIOU";
+    let lastVowel = false;
+    let position = 0;
+    for (let i=0; i <= word.length; i++) {
+        if (vowels.includes(word[i])) {
+            lastVowel = true;
+            position = i;
+        }
+    }
+
+    let firstStr = "";
+    let lastStr = "";
+    if (position >= 0) {
+        firstStr = word.substring(0,position);
+        lastStr = word.substring(position+1, word.length);
+        return firstStr + lastStr;
+    } else {
+        return word;
+    }
+
 }
 
 console.log(removeLastVowel('bootcamp')); // 'bootcmp'
