1161ff5d194733492dbb8666840401507e1fb7b9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-07 23:31:25 -0400

    feat: Implement removeEWords function to remove words containing the letter 'e'

---
+++ javascript-fundamentals/data-types/removeEWords.js
@@ -0,0 +1,7 @@
+// your code here
+function removeEWords(word) {
+
+}
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
