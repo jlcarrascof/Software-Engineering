97fe1fb89688c23d3d0c8f39d2260bd27dc1996a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-11 19:48:13 -0400

    feat: Implement addToArray function to add an element to an array at a specified location

---
+++ javascript-fundamentals/add-to-arrays/addToArray.js
@@ -0,0 +1,15 @@
+function addToArray(location, element, arr) {
+    // Your code here
+
+}
+
+testArray = [1,2,3]
+
+addToArray("FRONT", 0, testArray)
+console.log(testArray) // [0,1,2,3]
+
+addToArray("BACK", 4, testArray)
+console.log(testArray) // [0,1,2,3,4]
+
+addToArray("MIDDLE", 4, testArray) // "ERROR"
+console.log(testArray) // [0,1,2,3,4]
