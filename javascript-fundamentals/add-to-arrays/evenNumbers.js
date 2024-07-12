ea3245cbc32c8fc711b55bd730f3e5f2ff76a415
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-11 21:58:35 -0400

    feat: Add evenNumbers function to generate an array of even numbers within a specified range

---
+++ javascript-fundamentals/add-to-arrays/evenNumbers.js
@@ -0,0 +1,11 @@
+// your code here
+
+
+
+
+
+
+
+console.log(evenNumbers(7)); // [ 2, 4, 6 ]
+console.log(evenNumbers(12)); // [ 2, 4, 6, 8, 10 ]
+console.log(evenNumbers(15)); // [ 2, 4, 6, 8, 10, 12, 14 ]
