9e7bd4485e2efe63b29588a504c3d4e83a1db0e9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-11 22:05:12 -0400

    Refactor evenNumbers function for improved performance and readability

--- javascript-fundamentals/add-to-arrays/evenNumbers.js
+++ javascript-fundamentals/add-to-arrays/evenNumbers.js
@@ -1,10 +1,20 @@
 // your code here
+const evenNumbers = function(max) {
 
+    let arr = [];
 
+    if (max <= 1) {
+        return arr;
+    }
 
+    for (let i = 1; i < max; i++) {
+        if (i % 2 === 0) {
+            arr.push(i);
+        }
+    }
 
-
-
+    return arr;
+}
 
 console.log(evenNumbers(7)); // [ 2, 4, 6 ]
 console.log(evenNumbers(12)); // [ 2, 4, 6, 8, 10 ]
