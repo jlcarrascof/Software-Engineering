8ab65e2749f0e90e4ad13c179d778e15417cae82
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-12 19:01:46 -0400

    Refactor fizzBuzzArray.js to fix logic error in generating the array of numbers divisible by 5

--- javascript-fundamentals/add-to-arrays/fizzBuzzArray.js
+++ javascript-fundamentals/add-to-arrays/fizzBuzzArray.js
@@ -2,6 +2,22 @@
 
 function fizzBuzz(max) {
 
+    let arr = [];
+
+    for (let i=1; i < max; i++) {
+        if (i % 3 !== 0) {
+            if (i % 5 === 0) {
+                arr.push(i);
+            }
+        } else {
+            if (i % 5 !== 0) {
+                arr.push(i);
+            }
+        }
+    }
+
+    return arr;
+
 }
 
 console.log(fizzBuzz(12)); // [ 3, 5, 6, 9, 10 ]
