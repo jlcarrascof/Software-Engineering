e14ef784d6c418be85797ef58f9ba4bb61c7eeb2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-12 21:47:58 -0400

    feat: Add doubleSequence function to generate an array of numbers by doubling a given starting number with a specified length

---
+++ javascript-fundamentals/add-to-arrays/doubleSequence.js
@@ -0,0 +1,9 @@
+// Your code here
+
+
+
+console.log(doubleSequence(7, 3));  // [7, 14, 28]
+console.log(doubleSequence(3, 5));  // [3, 6, 12, 24, 48]
+console.log(doubleSequence(5, 3));  // [5, 10, 20]
+console.log(doubleSequence(5, 4));  // [5, 10, 20, 40]
+console.log(doubleSequence(5, 0));  // [ ]
