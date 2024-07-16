98b9c32f49e9b27cb734abe58362d5f55f5fd7ba
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-16 11:27:29 -0400

    Refactor tripleSequence function for resolved and improved performance and readability

--- javascript-fundamentals/add-to-arrays/tripleSequence.js
+++ javascript-fundamentals/add-to-arrays/tripleSequence.js
@@ -1,6 +1,18 @@
 function tripleSequence(start, length) {
     // your code here
 
+    let arrResult = [];
+    let factor = start;
+
+    for (let i=0; i < length; i++) {
+        if (i !== 0) {
+            factor = factor * 3;
+        }
+        arrResult.push(factor);
+    }
+
+    return arrResult;
+
 }
 
 console.log(tripleSequence(2, 4)); // [2, 6, 18, 54]
