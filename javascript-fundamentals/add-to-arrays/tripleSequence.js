012901ee837cb8322bee5170647c0ccf3bf34537
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-16 11:14:23 -0400

    feat: Add tripleSequence function to generate an array of numbers by tripling a given starting number with a specified length

---
+++ javascript-fundamentals/add-to-arrays/tripleSequence.js
@@ -0,0 +1,7 @@
+function tripleSequence(start, length) {
+    // your code here
+
+}
+
+console.log(tripleSequence(2, 4)); // [2, 6, 18, 54]
+console.log(tripleSequence(4, 5)); // [4, 12, 36, 108, 324]
