0a05854ee9f8d1b3ef301dd4c0d0c72c07870e62
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-16 11:32:39 -0400

    feat: Add unique function to generate an array of unique elements from an input array

---
+++ javascript-fundamentals/add-to-arrays/unique.js
@@ -0,0 +1,7 @@
+// Your code here
+
+
+
+console.log(unique([1, 1, 2, 3, 3])); // [1, 2, 3]
+console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
+console.log(unique(['a', 'b', 'c', 'b'])); // ['a', 'b', 'c']
