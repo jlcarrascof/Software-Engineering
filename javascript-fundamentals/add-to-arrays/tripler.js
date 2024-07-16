7baa9f292bf5d0813389ef8a180a80921a2545c9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-16 11:58:09 -0400

    Refactor tripler function for improved performance and readability

--- javascript-fundamentals/add-to-arrays/tripler.js
+++ javascript-fundamentals/add-to-arrays/tripler.js
@@ -1,9 +1,15 @@
 // your code here
+const tripler = function(arr) {
 
+    let finalArray = [];
 
+    for (let i=0; i < arr.length; i++) {
+        finalArray.push(arr[i] * 3);
+    }
 
+    return finalArray;
 
-
+}
 
 console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
