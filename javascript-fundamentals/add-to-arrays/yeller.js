d7a03eee5abc784d3b4f0a3283d1cfe689c6b375
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-16 11:53:37 -0400

    Refactor and resolve yeller function for improved performance and readability

--- javascript-fundamentals/add-to-arrays/yeller.js
+++ javascript-fundamentals/add-to-arrays/yeller.js
@@ -1,7 +1,15 @@
 // your code here
+const yeller = function(arr) {
 
+    let finalArray = [];
 
+    for (let i=0; i < arr.length; i++) {
+        finalArray.push(arr[i].toUpperCase() + "!");
+    }
 
+    return finalArray;
+
+}
 
 console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
 console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
