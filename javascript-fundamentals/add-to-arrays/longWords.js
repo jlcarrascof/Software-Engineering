87a6bebd724b0b35a4b7c2dc51374cc6922f279f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-17 20:33:28 -0400

    feat: Add longWords function to filter out words with less than 8 characters from an array

---
+++ javascript-fundamentals/add-to-arrays/longWords.js
@@ -0,0 +1,9 @@
+// your code here
+
+
+
+
+
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
