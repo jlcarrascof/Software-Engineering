7e68ca3d329d9788ce0b093d137fe4e9b41771a0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-17 20:55:04 -0400

    feat: Add chooseyEndings function to filter out words from an array that end with a specific suffix

---
+++ javascript-fundamentals/add-to-arrays/chooseyEndings.js
@@ -0,0 +1,18 @@
+// Your code here
+
+
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'ly'));
+// [ 'family', 'fly', 'timidly' ]
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'nd'));
+// [ 'hound', 'bond' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
+// [ 'simplicity', 'felicity' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
+// [ ]
+
+console.log(chooseyEndings(17, 'ily'));
+// [ ]
