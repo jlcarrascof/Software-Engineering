e2ef6e60a967ce3ca856488b91a6a3ef90f34866
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-18 23:22:30 -0400

    feat: Add commonFactors function to find the common factors of two numbers

---
+++ javascript-fundamentals/add-to-arrays/commonFactors.js
@@ -0,0 +1,11 @@
+// Your code here
+
+
+
+
+console.log(commonFactors(50, 30));            // [1, 2, 5, 10]
+console.log(commonFactors(8, 4));              // [1, 2, 4]
+console.log(commonFactors(4, 8));              // [1, 2, 4]
+console.log(commonFactors(12, 24));            // [1, 2, 3, 4, 6, 12]
+console.log(commonFactors(22, 44));            // [1, 2, 11, 22]
+console.log(commonFactors(7, 9));              // [1]
