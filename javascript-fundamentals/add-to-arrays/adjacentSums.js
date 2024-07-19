2269e0e89787de19d7b717684e6ccd3b095ad285
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-19 19:58:11 -0400

    feat: Add adjacentSums function to calculate sums of adjacent elements in an array

---
+++ javascript-fundamentals/add-to-arrays/adjacentSums.js
@@ -0,0 +1,8 @@
+// Your code here
+const adjacentSums = function(arr) {
+
+}
+
+console.log(adjacentSums([3, 8, 7, 1]));        // [ 11, 15, 8 ]
+console.log(adjacentSums([10, 5, 4, 3, 9]));    // [ 15, 9, 7, 12 ]
+console.log(adjacentSums([2, -3, 3]));
