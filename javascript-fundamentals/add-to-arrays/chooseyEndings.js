a17244f46c1b6112bc199202fc2e4472e27120f6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-18 22:52:57 -0400

    Refactor chooseyEndings function to handle non-array input and create commonFactors.js file

--- javascript-fundamentals/add-to-arrays/chooseyEndings.js
+++ javascript-fundamentals/add-to-arrays/chooseyEndings.js
@@ -16,18 +16,13 @@ const chooseyEndings = function(arr, suffix) {
 
 }
 
-
 console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'ly'));
 // [ 'family', 'fly', 'timidly' ]
-
 console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'nd'));
 // [ 'hound', 'bond' ]
-
 console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
 // [ 'simplicity', 'felicity' ]
-
 console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
 // [ ]
-
 console.log(chooseyEndings(17, 'ily'));
 // [ ]
