b1451757241353aad1e7758b9210be5c8d45952e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-20 22:25:52 -0400

    feat: Add fibonacciSequence function to generate Fibonacci sequence

---
+++ javascript-fundamentals/add-to-arrays/fibonacciSequence.js
@@ -0,0 +1,12 @@
+// Your code here
+
+const fibonacciSequence = function(limit) {
+
+}
+
+console.log(fibonacciSequence(4));  // [ 1, 1, 2, 3 ]
+console.log(fibonacciSequence(5));  // [ 1, 1, 2, 3, 5 ]
+console.log(fibonacciSequence(8));  // [ 1, 1, 2, 3, 5, 8, 13, 21 ]
+console.log(fibonacciSequence(0));  // [ ]
+console.log(fibonacciSequence(1));  // [ 1 ]
+console.log(fibonacciSequence(2));  // [ 1, 1 ]
