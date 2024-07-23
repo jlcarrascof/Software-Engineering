350e67e31f2fede73262c91804ad2c139ed5ba5e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-23 18:45:07 -0400

    feat: Add greatestFactorArray function to find the greatest factor of each element in an array

---
+++ javascript-fundamentals/add-to-arrays/greatestFactorArray.js
@@ -0,0 +1,6 @@
+function greatestFactorArray(array) {
+    // your code here
+}
+
+console.log(greatestFactorArray([16, 7, 9, 14])); // [8, 7, 9, 7]
+console.log(greatestFactorArray([30, 3, 24, 21, 10])); // [15, 3, 12, 21, 5]
