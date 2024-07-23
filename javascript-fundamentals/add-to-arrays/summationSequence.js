c78721a3ab3961f4c70c42fdec69dd8b9f1ee638
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-23 19:58:25 -0400

    feat: Add summationSequence function to generate a sequence of summations starting from a given number

---
+++ javascript-fundamentals/add-to-arrays/summationSequence.js
@@ -0,0 +1,6 @@
+function summationSequence(start, length) {
+    // your code here
+}
+
+console.log(summationSequence(3, 4)); // [3, 6, 21, 231]
+console.log(summationSequence(5, 3)); // [5, 15, 120]
