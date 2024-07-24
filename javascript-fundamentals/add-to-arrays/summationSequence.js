cbbd8fd567c2ef086719ac1693e5349c8ce7b1c2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-23 20:51:11 -0400

    Refactor and resolve summationSequence function for improved performance and readability

--- javascript-fundamentals/add-to-arrays/summationSequence.js
+++ javascript-fundamentals/add-to-arrays/summationSequence.js
@@ -1,5 +1,23 @@
 function summationSequence(start, length) {
     // your code here
+    let resultArray = [start];
+
+    for (let i=1; i < length; i++) {
+        previous = resultArray[i - 1];
+        adding = calcAdding(previous);
+        resultArray.push(adding);
+    }
+
+    return resultArray;
+}
+
+function calcAdding(number) {
+    sum = 0;
+    for (let i=1; i <= number; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 console.log(summationSequence(3, 4)); // [3, 6, 21, 231]
